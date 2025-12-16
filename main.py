82aca6ec4573284e91bccf68cc44ab81abd13503
chengw <260323117@qq.com>
2025-12-16 16:51:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import datetime
 now = datetime.datetime.utcnow()
 print("Hello GitHub Actions!")
 print("Current UTC time:", now)
+
+import os
+
+api_key = os.getenv("BINANCE_API_KEY")
+secret = os.getenv("BINANCE_SECRET")
+
+print("BINANCE_API_KEY loaded:", bool(api_key))
+print("BINANCE_SECRET loaded:", bool(secret))
